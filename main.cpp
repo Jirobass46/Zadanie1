508e9e6002d9775c848f67745087f5f3d63bfce4
Jirobass46 <107275603+Jirobass46@users.noreply.github.com>
2022-12-12 15:13:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 template <typename X>
 class score
@@ -8,52 +9,66 @@ public:
     score(){a = 0; b = 0;};
     void plus(X a, X b)
     {
+ 
         cout << a + b;
     }
-
+ 
     void minus(X a, X b)
     {
+ 
         cout << a - b;
     }
-
+ 
     void proizv(X a, X b)
     {
         cout << a * b;
     }
+ 
+    double proizv(double a, double b)
+    {
+        cin >> a;
+        cin >> b;
+        return pow(a,b);
+    }
 };
-
+ 
 int main()
 {
     int a, b, x;
-    score <int> thing;
-    cout << "First variable - ";
-    cin >> a;
-    cout << "Second variable - ";
-    cin >> b;
-    cout << "Select an action: " << endl << "1.Addition" << endl << "2.Subtraction" << endl << "3.Multiplication" << endl;
-    cin >> x;
-    switch(x)
-    {
-    case 1:{
-        cout << "Result - " << endl;
-        thing.plus(a,b);
-        cout << endl;
-     break;
-       }
-    case 2:{
-         cout << "Result - " << endl;
-         thing.minus(a,b);
-         cout << endl;
-     break;
-       }
-    case 3:{
-        cout << "Result - ";
-        thing.proizv(a,b);
-        cout << endl;
-     break;
-       }
-    if (x > 3 || x < 1)
-        cout << "After all, it is written that there are only 3 operations, do not break the program, please" << endl;
-}
-    return 0;
-}
+        score <int> thing;
+        cout << "First variable - ";
+        cin >> a;
+        cout << "Second variable - ";
+        cin >> b;
+        cout << "Select an action: " << endl << "1.Addition" << endl << "2.Subtraction" << endl << "3.Multiplication" << endl
+             << "4. Erect"<< endl;
+        cin >> x;
+        switch(x)
+        {
+        case 1:{
+ 
+            cout << "Result - " << endl;
+            thing.plus(a,b);
+            cout << endl;
+         break;
+           }
+        case 2:{
+             cout << "Result - " << endl;
+             thing.minus(a,b);
+             cout << endl;
+         break;
+           }
+        case 3:{
+            cout << "Result - ";
+            thing.proizv(a,b);
+            cout << endl;
+         break;
+        }
+        case 4:{
+                cout << "Result - ";
+                thing.proizv(a,b);
+                cout << endl;
+                }
+        }
+        return 0;
+    }
